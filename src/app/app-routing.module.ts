8491cafc2be630a189c124874ae7a52354ae1356
Trevor Sawler <trevor.sawler@gmail.com>
2020-07-23 12:49:37 -0300

    Adding sub pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-    }
+    },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
+  }
 ];
 
 @NgModule({
