0719df157d1f199f7e87c3c673b77c9e4c50cd40
Trevor Sawler <trevor.sawler@gmail.com>
2020-07-24 11:25:42 -0300

    problem detail pages

--- src/app/hosts/hosts-page.component.ts
+++ src/app/hosts/hosts-page.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {HostModel} from './host.model';
 import {HostsServiceService} from './hosts-service.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-hosts-component',
@@ -11,10 +12,14 @@ export class HostsPage {
 
     hosts: HostModel[];
 
-    constructor(private hostsService: HostsServiceService) {
+    constructor(private hostsService: HostsServiceService, private router: Router) {
     }
 
     ionViewWillEnter() {
         this.hosts = this.hostsService.getAllHosts();
+    }
+
+    onEdit(id: number) {
+        this.router.navigateByUrl('/tabs/hosts/edit-host/' + id);
     }
 }
